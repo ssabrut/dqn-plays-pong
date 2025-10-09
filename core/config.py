cfcfaac679d83b0d882ef821c62553b1c5fe4e9d
Michael Eco <michael.gunawan2002@gmail.com>
2025-10-09 11:18:20 +0700

    feat: added hyperparameter and config settings

---
+++ core/config.py
@@ -0,0 +1,20 @@
+import torch
+
+# --- Environment ---
+ENV_NAME: str = "ALE/Pong-v5"
+MODEL_SAVE_PATH: str = "models/dqn_pong_model.pth"
+DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# --- Hyperparameters ---
+BATCH_SIZE: int = 32
+GAMMA: float = 99e-2
+EPSILON_START: float = 1.
+EPSILON_END: float = 2e-2
+EPSILON_DECAY: int = 30000
+TARGET_UPDATE_FREQ: int = 1000
+LEARNING_RATE: float = 1e-4
+BUFFER_SIZE: int = 100000
+
+# --- Training settings ---
+TOTAL_FRAMES: int = 1000000
+LOG_FREQ: int = 20 # log every 20 episodes
